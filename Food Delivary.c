658bfdddb69832aa08147edde780f9edd78d6475
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-10 02:07:55 +0530

    print item list

--- Food Delivary.c
+++ Food Delivary.c
@@ -3,5 +3,22 @@ int main(void)
 {
   printf("\nSPECIAL FOOD DELIVERY SERVICE FOR CURFEW AREAS\n");
   printf("Stay Home Protect our Community against COVID-19\n\n");
+  printf("------------------------------------------------\n");
+  printf("Food Id \t Food Name \t\t Qty \t Price(Rs:)\n");
+  printf("------------------------------------------------\n");
+  printf("A \t\t\t Samba Rice \t 3kg \t 270.00\n");
+  printf("B \t\t\t Milk Powder \t 200g \t 100.00\n");
+  printf("C \t\t\t Tea \t\t\t 100g \t 50.00\n");
+  printf("D \t\t\t Jack Mackeral \t 100g \t 100.00\n");
+  printf("E \t\t\t Biscuit \t\t 100g \t 50.00\n");
+  printf("F \t\t\t Coconut Oil \t 500ml \t 150.00\n");
+  printf("G \t\t\t Noodles \t\t 400g \t 110.00\n");
+  printf("H \t\t\t Soyameat \t\t 100g \t 70.00\n");
+  printf("I \t\t\t Onion \t\t\t 500g \t 80.00\n");
+  printf("J \t\t\t Potato \t\t 500g \t 90.00\n");
+  printf("K \t\t\t Eggs \t\t\t 5 \t\t 52.00\n");
+  printf("L \t\t\t Carrot \t\t 500g \t 160.00\n");
+  printf("M \t\t\t Sugar \t\t 100g \t 45.00\n");
+  printf("------------------------------------------------\n");
   return 0;
 }
