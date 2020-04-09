ae7ae96c91915d8839b8537589dc40875b00c47d
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-10 02:05:27 +0530

    Heading added

--- Food Delivary.c
+++ Food Delivary.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void) 
 {
-  
+  printf("\nSPECIAL FOOD DELIVERY SERVICE FOR CURFEW AREAS\n");
+  printf("Stay Home Protect our Community against COVID-19\n\n");
   return 0;
 }
