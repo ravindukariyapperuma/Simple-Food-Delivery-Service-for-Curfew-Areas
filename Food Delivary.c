2bd5c6301c64dbde41d48d2b43df916b2dea49fd
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-10 02:05:10 +0530

    create project

--- Food Delivary.c
+++ Food Delivary.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void) 
 {
-  printf("\nSPECIAL FOOD DELIVERY SERVICE FOR CURFEW AREAS\n");
-  printf("Stay Home Protect our Community against COVID-19\n\n");
+  
   return 0;
 }
